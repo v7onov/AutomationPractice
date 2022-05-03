49e9beeb599cb5cd842ce918ac30f16cebabb8cc
Victor Semyonov <v7onov@gmail.com>
2022-05-04 00:02:28 +0300

    annotations were added

--- src/test/java/tests/AddProductToTheCartTest.java
+++ src/test/java/tests/AddProductToTheCartTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AccountPage;
@@ -8,9 +9,17 @@ import pages.MainPage;
 import pages.SignInPage;
 import testdata.GetNewLoginModel;
 
+
+@Epic("UI regression tests")
+@Feature("Cart")
+@Story("Add one product to the cart test")
 public class AddProductToTheCartTest extends BaseTest {
 
+
     @Test
+    @Description("Check if adding of a product to the cart is possible")
+    @Attachment(value = "screenshot", type = "image/png")
+    @Severity(SeverityLevel.CRITICAL)
     public void addProductTest(){
         MainPage mainPage = new MainPage(driver);
         mainPage.openMainPage();
